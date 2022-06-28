702c5fc378f76238c0eabcabe140b799cbbccad0
Won Tae Lee <maxandjava@gmail.com>
2022-06-28 14:35:39 +0900

    completed roughly

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-rm -r 1e-05
+
 
 decomposePar -force
-mpirun -np 8   buoyantPimpleFoam > log. buoyantPimpleFoam -parallel
+mpirun -np 8   buoyantPimpleFoam > log.buoyantPimpleFoam -parallel
 # mpirun -np 8  buoyantPimpleFoam  > /dev/null -parallel
 reconstructPar
 
